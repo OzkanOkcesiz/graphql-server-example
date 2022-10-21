f8c23148ef1e7b6ce23ef0193e5cac6bf8ba7da2
Ozkan <ozkan.okcesiz@gmail.com>
2022-10-21 03:29:06 +0300

    add typeDefs & resolvers

---
+++ data.js
@@ -0,0 +1,63 @@
+const users = [
+    {
+        id: "1",
+        fullName: "Önder ŞAHİN",
+        age:32
+    },
+    {
+        id: "2",
+        fullName: "Berkin OKTAY",
+        age: 23
+    }
+];
+
+const posts = [
+    {
+        id: "1",
+        title: "Önder'in gönderisi",
+        user_id: "1"
+    },
+    {
+        id: "2",
+        title: "Önder'in diğer gönderisi",
+        user_id: "1"
+    },
+    {
+        id: "3",
+        title: "Berkin'in gönderisi",
+        user_id: "2"
+    }
+];
+
+const comments = [
+    {
+        id: "1",
+        text: "Lorem ipsum",
+        post_id: "1",
+        user_id: "2"
+    },
+    {
+        id: "2",
+        text: "Lorem ipsum doler",
+        post_id: "1",
+        user_id: "1"
+    },
+    {
+        id: "3",
+        text: "foo bar",
+        post_id: "2",
+        user_id: "2"
+    },
+    {
+        id: "4",
+        text: "foo bar baz",
+        post_id: "3",
+        user_id: "1"
+    }
+];
+
+module.exports = {
+    users,
+    posts,
+    comments
+}
